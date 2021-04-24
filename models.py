c3c107dd0a0e6b21afc9e97bd1326c33f1447f1c
alex <alex@kali.kalinet>
2021-04-24 13:07:47 +0300

    Add login and some routes

--- models.py
+++ models.py
@@ -1,13 +1,8 @@
-from datetime import datetime
 from app import db, login
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from base64 import b64encode
 from flask_login import UserMixin
-from app import login
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+from base64 import b64encode
 
 
 class User(UserMixin, db.Model):
@@ -18,6 +13,13 @@ class User(UserMixin, db.Model):
 	password_hash = db.Column(db.String(128))
 	avatar = db.Column(db.LargeBinary)
 	about_me = db.Column(db.String(140))
+	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+	followed = db.relationship(
+		'User', secondary = followers,
+		primaryjoin = (followers.c.follower_id == id),
+		secondaryjoin = (followers.c.followed_id == id),
+		backref = db.backref('followers', lazy = 'dynamic'), lazy = 'dynamic'
+		)
 	def set_password(self, password):
 		self.password_hash = generate_password_hash(password)
 	def check_password(self, password):
