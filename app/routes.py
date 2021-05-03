aa71471c8313fd327be0a97eb95151de2318e0ba
Tuman-42 <zverv.Sasha@yandex.ru>
2021-05-03 15:48:57 +0300

    PublicVercion

--- app/routes.py
+++ app/routes.py
@@ -4,11 +4,178 @@ from app.models import User
 from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm, RegistrationForm, ResponsibilityWaste, EditProfileForm, ResponsibilityWaste_alt, TestForm
 from flask_login import current_user, login_user, logout_user, login_required
-from test1 import test1
 
 '''
 Функция логина
 '''
+class test1():
+
+    ask = { 0: ("В ходе работы Вашего заведения образуются перерабатываемые бумажные отходы?", ["Да", "Нет"]),
+            1: ("Ваше заведение сдаёт макулатуру?", [ "Да", "Нет"]),
+            2: ("Какая часть бумажных отходов перерабатывается?", ["Менее 30%", "Оклоло 50%", "Более 70%"]),
+           
+            3: ("В ходе работы Вашего заведения образуются отходы, которые можно переработать вместе с металлом (алюминий, фольгу, металлические предметы и упаковку)?", [ "Да", "Нет"]),           
+            4: ("Ваше заведение сдаёт отходы такого типа на переработку?", [ "Да", "Нет"]),
+            5: ("Какая часть отходов перерабатывается?", ["Менее 30%", "Оклоло 50%", "Более 70%"]),
+           
+            6: ("В ходе работы Вашего заведения образуются пластиковые отходы?", [ "Да", "Нет"]),
+            7: ("Ваше заведение сдаёт отходы такого типа на переработку?", [ "Да", "Нет"]),
+            8: ("Какая часть отходов перерабатывается?", ["Менее 30%", "Оклоло 50%", "Более 70%"]),
+            
+            9: ("В ходе работы Вашего заведения образуются отходы тетрапак и их аналоги?", [ "Да", "Нет"]),
+            10: ("Ваше заведение сдаёт отходы такого типа на переработку?", [ "Да", "Нет"]),
+            11: ("Какая часть отходов перерабатывается?", ["Менее 30%", "Оклоло 50%", "Более 70%"]),
+            
+            12: ("В ходе работы Вашего заведения образуются отходы из стекла?", [ "Да", "Нет"]),
+            13: ("Ваше заведение сдаёт отходы такого типа на переработку?", [ "Да", "Нет"]),
+            14: ("Какая часть отходов перерабатывается?", ["Менее 30%", "Оклоло 50%", "Более 70%"]),
+
+
+            15: ("Ваше заведение сдаёт на переработку вышедшие из строя расходные материалы (батарейки, аккумуляторы, ртутные лампы)?", ["Да", "Нет", "Только часть", "Заведение не использует подобное"]),
+            
+            
+            'end': ("",[])}
+
+    def __init__(self):
+        self.question = 0
+        self.recomend = ""
+        self.result_recykle = 0
+
+    def start(self):
+        return test1.ask[0]
+
+    def asked(self, question, answer):
+        self.question = question
+        return self.next(answer)
+
+    def next(self, choice):
+        
+        if self.question == 0:
+            if choice == 0:
+                self.question = 1
+            else:
+                self.result_recykle += 5
+                self.question  = 3
+        
+        elif self.question == 1:
+            if choice == 0:
+                self.question = 2
+            else:
+                self.question = 3
+        
+        elif self.question == 2:
+            self.question = 3
+            if choice == 0:
+                self.result_recykle += 1
+            elif choice == 1:
+                self.result_recykle += 3
+            else:
+                self.result_recykle += 5
+
+        elif self.question == 3:
+            if choice == 0:
+                self.question = 4
+            else:
+                self.result_recykle += 5
+                self.question = 6
+        
+        elif self.question == 4:
+            if choice == 0:
+                self.question = 5
+            else:
+                self.question = 6
+        
+        elif self.question == 5:
+            self.question = 6
+            if choice == 0:
+                self.result_recykle += 1
+            elif choice == 1:
+                self.result_recykle += 3
+            else:
+                self.result_recykle += 5
+        
+        
+        elif self.question == 6:
+            if choice == 0:
+                self.question = 7
+            else:
+                self.result_recykle += 5
+                self.question = 9
+        
+        elif self.question == 7:
+            if choice == 0:
+                self.question = 8
+            else:
+                self.question = 9
+        
+        elif self.question == 8:
+            self.question = 9
+            if choice == 0:
+                self.result_recykle += 1
+            elif choice == 1:
+                self.result_recykle += 3
+            else:
+                self.result_recykle += 5
+
+        elif self.question == 9:
+            if choice == 0:
+                self.question = 10
+            else:
+                self.result_recykle += 5
+                self.question = 12
+        
+        elif self.question == 10:
+            if choice == 0:
+                self.question = 11
+            else:
+                self.question = 12
+        
+        elif self.question == 11:
+            self.question = 12
+            if choice == 0:
+                self.result_recykle += 1
+            elif choice == 1:
+                self.result_recykle += 3
+            else:
+                self.result_recykle += 5
+
+        elif self.question == 12:
+            if choice == 0:
+                self.question = 13
+            else:
+                self.result_recykle += 5
+                self.question = 15
+        
+        elif self.question == 13:
+            if choice == 0:
+                self.question = 14
+            else:
+                self.question = 15
+        
+        elif self.question == 14:
+            self.question = 15
+            if choice == 0:
+                self.result_recykle += 1
+            elif choice == 1:
+                self.result_recykle += 3
+            else:
+                self.result_recykle += 5
+
+        elif self.question == 15:
+            self.question = 16 
+            if choice == 2:
+                self.result_recykle += 3
+            if choice in [0, 3]:
+                self.result_recykle += 6
+            if self.result_recykle < 12:
+                self.recomend += "Ваше заведение уделяет несдостаточно внимания переработке непищевых отходов\n"
+            elif self.result_recykle < 17:
+                self.recomend += "Ваше заведение, активно участвует в переработке "
+
+        return test1.ask[self.question]
+
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -176,15 +343,17 @@ def quiz():
 @login_required
 def quiz_alt():
     userTest = test1()
-    userLog = current_user.answers   
+    userLog = current_user.answers
     forForm = userTest.start()
     if userLog == None:
         pass
     else:
         for ans in userLog:
-            forForm = userTest.next(int(ans)) 
-    if 0:
-        "Переход на страницу с результатом"
+            forForm = userTest.next(ans) 
+    if userTest.question == 'end':
+        current_user.ecological = (userTest.result_food + userTest.result_recykle)/26
+        db.session.commit()    
+        return redirect(url_for('company', companyname=current_user.username))
     else: #подать в формму нужный вопрос
         form = ResponsibilityWaste_alt()
         form.fff(forForm)
@@ -193,9 +362,8 @@ def quiz_alt():
                 current_user.answers = str(forForm[1].index(form.first.data))
             else:
                 current_user.answers += str(forForm[1].index(form.first.data))
-            db.session.commit()
-            #сохранить ответ пользователя 
+            db.session.commit()    
             flash(form.first.data)     
-            return render_template('onetask.html', form = form, title = 'Ecoquiz')
-        return render_template('onetask.html', form = form, title = 'Ecoquiz')
-        # возможность узнать свою экооценку
+            return redirect(url_for('quiz_alt'))
+        return render_template('onetask.html', form = form, text='test', title = 'Ecoquiz')
+# возможность узнать свою экооценку
